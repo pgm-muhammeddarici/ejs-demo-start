beacb230aa5b427e2feeeb8fe3e18fd80541dae2
pgm-muhammeddarici <mhmmmd.drc.45@gmail.com>
2025-02-18 11:09:10 +0100

    person rendered

--- src/controllers/PageController.js
+++ src/controllers/PageController.js
@@ -1,3 +1,5 @@
+import { person } from "../data/data.js";
+
 export const home = (req, res) => {
     res.render("pages/home", {
         title: "Dinosaurs around the world",
@@ -9,14 +11,18 @@ export const about = (req, res) => {
         title: "About us",
         content: "We are group of dinosaur enthusiasts",
         team: [
-            "T-rex", "Velociraptor", "Stegosaurus",
-        ]
+            "T-rex",
+            "Velociraptor",
+            "Stegosaurus",
+            "<strong>Premium</strong> Diplodocus",
+        ],
     });
 };
 export const contact = (req, res) => {
     res.render("pages/contact", {
         title: "Contact",
         content: "Contact us at 09 233 40 89",
+        person,
     });
 };
 export const privacy = (req, res) => {
